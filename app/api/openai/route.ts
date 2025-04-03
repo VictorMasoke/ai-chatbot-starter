97c7c0b8bff637c17fc2054233d7ce5ab57980be
Victor Masoke <123163027+VictorMasoke@users.noreply.github.com>
2025-04-03 09:46:09 +0200

    Basic Chatbot with GEMINI Done

---
+++ app/api/openai/route.ts
@@ -0,0 +1,20 @@
+import { streamText, Message } from "ai";
+import { createOpenAI } from "@ai-sdk/openai";
+import { initialMessage } from "@/lib/data";
+
+const openai = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY || "",
+  compatibility: "strict"
+});
+
+export const runtime = "edge";
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+  const stream = await streamText({
+    model: openai("gpt-3.5-turbo"),
+    messages: [initialMessage, ...messages],
+    temperature: 0.7,
+  });
+  return stream.toDataStreamResponse();
+}
---
+++ lib/data.ts
@@ -0,0 +1,4 @@
+export const initialMessage = {
+    role: "system",
+    content: `You are a AI assistant for Ctrade a trading platform in Zimbabwe`
+}
